ec394737410ed9d9ad5b0dcce6a4662222c2c7f2
mmuchsin <kangucin21@gmail.com>
2025-10-24 08:22:40 +0700

    feat: integrate Google Fonts for improved typography and remove redundant font import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,22 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-05-07",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', "@nuxt/icon", "@vueuse/nuxt"],
+  modules: [
+    '@nuxt/ui',
+    "@nuxt/icon",
+    "@vueuse/nuxt",
+    "@nuxtjs/google-fonts",
+  ],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+  googleFonts: {
+    families: {
+      'Scheherazade New': [400, 500, 600, 700]
+    },
+    display: 'swap'
+  }
 })
